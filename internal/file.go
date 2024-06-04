0d45413ec75ac7b2b6846259dbd5d0d55cfea452
Ikuya Nagaki <ikuya313198@gmail.com>
2024-06-04 17:18:10 +0900

    feat: add func

--- internal/file.go
+++ internal/file.go
@@ -1,12 +1,49 @@
 package internal
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
-func CreateFile(path string, content string) error {
-	data := []byte(content)
-	err := os.WriteFile(path, data, 0644)
+type File struct {
+	Path    string
+	Content string
+}
+
+func NewFile(path, content string) *File {
+	return &File{
+		Path:    path,
+		Content: content,
+	}
+}
+
+func CreateFile(file File) error {
+	content := []byte(file.Content)
+	err := os.WriteFile(file.Path, content, 0644)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func ExploreFiles(path string) ([]File, error) {
+	var files []File
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		contentInBytes, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		files = append(files, File{Path: path, Content: string(contentInBytes)})
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
 }
